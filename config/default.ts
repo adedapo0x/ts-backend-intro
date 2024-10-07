9a4bc05dd829713f4b5b418d2566728d10e5aca4
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-07 22:36:22 +0100

    user model created

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
     port: 3002,
-    dbUri: "mongodb://localhost:27017/backend-intro"
+    dbUri: "mongodb://localhost:27017/backend-intro",
+    saltNo: 10
 }
---
+++ src/models/user.models.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, Document } from "mongoose"
+import bcrypt from "bcrypt"
+import config from "config"
+import logger from "../utils/logger"
+
+interface IUser extends Document{
+    name: string
+    email: string
+    password: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+const userSchema = new Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}, {
+    timestamps: true
+})
+
+userSchema.pre('save', async function(next){
+    let user = this as IUser 
+    if (!user.isModified("password")){
+        next()
+    }
+    try{
+        const salt = await bcrypt.genSalt(config.get<number>('saltNo'))
+        user.password = await bcrypt.hashSync(user.password, salt)
+    } catch(e){
+        logger.error("Error encountered while hashing password")
+        throw e
+    }
+})
+
+userSchema.methods.comparePasswords = async function(pwd: string):Promise<boolean>{
+    let user = this as Document
+    try{
+        return await bcrypt.compare(pwd, user.password)
+    } catch(e){
+        logger.error("Error occured while comparing passwords")
+        return false
+    }
+}
+
+const User = mongoose.model<IUser>("User", userSchema)
+
+export default User
