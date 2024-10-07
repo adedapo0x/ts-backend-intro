7a1a54d16c755aff913cbfdf802cff3d1e294219
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-07 19:23:48 +0100

    zod validation setup complete

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express"
 import config from "config"
 import connectDB from "./utils/connectDB"
 import logger from "./utils/logger"
+import userRouter from "./routes/user.routes"
 
 const app =express()
 const port = config.get<number>("port")
 
+app.use(userRouter)
 
 app.listen(port, async () => {
     await connectDB()
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express"
+
+const router = express.Router()
+
+router.get('/healthcheck', (req: Request, res: Response) => {
+    res.sendStatus(200)}) 
+
+export default router
