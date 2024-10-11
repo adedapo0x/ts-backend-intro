147bf0925467b9424499599678f3cc8a83832d81
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-11 15:39:18 +0100

    resolved bug in validate middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,16 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { AnyZodObject } from "zod";
 
-const validate = (schema: AnyZodObject) => (req: Request, res: Response, next:NextFunction) => {
+const validate = (schema: AnyZodObject) => async (req: Request, res: Response, next:NextFunction):Promise<void> => {
     try{
-        schema.parse({
+        await schema.parseAsync({
             body: req.body,
             query: req.query,
             params: req.params
         })
-    } catch(e:any){
-        return res.status(400).send(e.errors)
+        next()
+    } catch(e: any){
+        res.status(400).json({
+            status: "error",
+            message: "Invalid input",
+            details: e.errors
+        })
     }
 }
-
 export default validate
