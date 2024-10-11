9740248056facef7b0e355bda188f9f9166eb63b
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-11 15:40:12 +0100

    added general error handler

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,21 @@ import config from "config"
 import connectDB from "./utils/connectDB"
 import logger from "./utils/logger"
 import userRouter from "./routes/user.routes"
+import { Request, Response, NextFunction } from "express"
 
 const app =express()
-const port = config.get<number>("port")
+const port = config.get<number>("port") || 1337
 
 app.use(userRouter)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.stack)
+    res.status(500).json({
+        status: "error",
+        message: "An unexpected error occured"
+    })
+
+})
 
 app.listen(port, async () => {
     await connectDB()
