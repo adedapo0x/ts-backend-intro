3899bbcebfbbafb0a52461f3285ecd41c05712d6
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-01 13:03:49 +0100

    implemented access token generation

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,7 +1,8 @@
 import {validatePassword} from "../middlewares/password-validate";
 import {Request, Response} from "express"
 import Session from "../models/session.model";
-
+import {signJWT} from "../utils/jwt";
+import config from "config"
 
 interface InputRequestBody {
     name: string;
@@ -9,8 +10,6 @@ interface InputRequestBody {
     password: string;
     confirmPassword: string;
 }
-
-
 
 export const createSession = async (userID:string, userAgent:string) => {
     const session = await Session.create({user: userID, userAgent})
@@ -23,7 +22,13 @@ export async function createUserSession(req: Request<{}, {}, InputRequestBody>, 
         const user= await validatePassword(req.body)
         if (!user) return res.status(401).json({message: "Invalid email or password"})
 
-        const session = createSession(user._id.toString(), req.get("user-agent") || "")
+        // create session
+        const session = await createSession(user._id.toString(), req.get("user-agent") || "")
+
+        // create access token
+        const accessToken = signJWT({...user, session: session._id}, {expiresIn: config.get("accessTokenTtl")})
+
+        // create refresh Token
 
 
     } catch (e) {
