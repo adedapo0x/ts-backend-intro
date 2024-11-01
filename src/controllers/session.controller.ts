743de4f5d2e0590ae010cb3c9066df8ef61b43d2
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-01 10:59:04 +0100

    added implementation of createSession function

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,9 +1,28 @@
 import {validatePassword} from "../middlewares/password-validate";
+import {Request, Response} from "express"
+import Session from "../models/session.model";
 
-const createUserSession = async (req: Request, res: Response) => {
+interface InputRequestBody {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export const createSession = async (userID:string, userAgent:string) => {
+    const session = await Session.create({user: userID, userAgent})
+    return session.toJSON()
+}
+
+export async function createUserSession(req: Request<{}, {}, InputRequestBody>, res: Response) {
     try{
         // Validates user password
-        const user = validatePassword(req.body)
+        const user= await validatePassword(req.body)
+        if (!user) return res.status(401).json({message: "Invalid email or password"})
+
+        const session = createSession(user._id.toString(), req.get("user-agent") || "")
+
+
     } catch (e) {
 
     }
