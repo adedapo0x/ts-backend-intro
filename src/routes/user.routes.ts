401ae34d15bff08c7a5fae3722a101f8fbd30282
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-08 13:02:14 +0100

    added route for session creation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,9 +2,12 @@ import express from "express"
 import reqValidate from "../middlewares/req-validate"
 import { httpRegister } from "../controllers/user.controller"
 import { createUserSchema } from "../schema/user.schema"
+import {createSessionSchema} from "../schema/session.schema";
+import {createUserSession} from "../controllers/session.controller";
 
 const router = express.Router()
 
 router.post('/api/users', reqValidate(createUserSchema), httpRegister)
+router.post("/api/session", reqValidate(createSessionSchema), createUserSession)
 
 export default router
