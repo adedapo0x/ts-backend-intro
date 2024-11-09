105383231ac926378d7697a4c4585b9dd54bbbf9
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-09 22:57:44 +0100

    restructured reqValidate middleware

--- src/middlewares/req-validate.ts
+++ src/middlewares/req-validate.ts
@@ -2,24 +2,25 @@ import { Request, Response, NextFunction } from "express";
 import {AnyZodObject, ZodError} from "zod";
 import  logger from "../utils/logger"
 
-const reqValidate = (schema: AnyZodObject) => async (req: Request, res: Response, next:NextFunction):Promise<void> => {
-    try{
-        await schema.parseAsync({
-            body: req.body,
-            query: req.query,
-            params: req.params
-        })
-        next()
-    } catch(e){
-        logger.error(e)
-        if (e instanceof ZodError){
-            res.status(400).json({message: "Validation error", error: e.issues})
+const reqValidate = (schema: AnyZodObject) =>
+    async (req: Request, res: Response, next:NextFunction)  => {
+        try{
+            await schema.parseAsync({
+                body: req.body,
+                query: req.query,
+                params: req.params
+            })
+            next()
+        } catch(e){
+            logger.error(e)
+            if (e instanceof ZodError){
+                return res.status(400).json({message: "Validation error", error: e.issues})
+            }
+            return res.status(400).json({
+                status: "error",
+                message: "An unexpected error occurred!",
+            })
         }
-        res.status(400).json({
-            status: "error",
-            message: "An unexpected error occurred!",
-        })
-    }
 }
 
 export default reqValidate
