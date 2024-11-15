3f8e04f008fe3430e03c36eda34e1172e2add20e
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-15 11:02:47 +0100

    changed reqBody type

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,22 +4,22 @@ import User from "../models/user.models"
 import { UserInputType } from "../schema/user.schema"
 import { omit } from "lodash"
 
-export const httpRegister = async (req: Request<{}, {}, UserInputType["body"]>, res: Response) => {
+export const httpRegister = async (req: Request<{}, {}, UserInputType>, res: Response) => {
     try {
         const { email } = req.body
         // check if email ,already exists
         const existingUser = await User.findOne({email})
         if (existingUser) {
-            return res.status(400).json({message: "Email already in use, try another!"})
+            res.status(400).json({message: "Email already in use, try another!"})
         }
 
         const user = await User.create(req.body)
-        return res.status(201).json({
+        res.status(201).json({
             message: "User created successfully!",
             created_user: omit(user.toJSON(), "password")
         })
     } catch (e) {
         logger.error(e)
-        return res.status(400).json({message: "Error encountered while creating user"})
+        res.status(400).json({message: "Error encountered while creating user"})
     }
 }
