c29bbb5a5e3e841d4cfa77aeb203b539e56f5430
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-15 11:03:54 +0100

    separated zodObject implementation from validationSchema type I set

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,23 +1,26 @@
 import z, { object, string } from "zod"
+import { validationSchema} from "../middlewares/req-validate";
 
-export const createUserSchema = object({
-    body: object({
-        name: string({
-            required_error: "Name is required"
-        }).min(3, "Name must have a minimum length of 3 characters"),
-        email: string({
-            required_error: "Email is required"
-        }).email("Please enter a valid email"),
-        password: string({
-            required_error: "Password is required"
-        }).min(6, "Password must have a minimum length of 6 characters"),
-        confirmPassword: string({
-            required_error: "Password is required"
-        })
+export const createUserBodySchema = object({
+    name: string({
+        required_error: "Name is required"
+    }).min(3, "Name must have a minimum length of 3 characters"),
+    email: string({
+        required_error: "Email is required"
+    }).email("Please enter a valid email"),
+    password: string({
+        required_error: "Password is required"
+    }).min(6, "Password must have a minimum length of 6 characters"),
+    confirmPassword: string({
+        required_error: "Password is required"
+    })
     }).refine((data):boolean => data.password === data.confirmPassword, {
         message: "Passwords do not match",
         path: ["confirmPassword"]
     })
-})
 
-export type UserInputType = z.infer<typeof createUserSchema>
+export const createUserSchema: validationSchema = {
+    body: createUserBodySchema
+}
+
+export type UserInputType = z.infer<typeof createUserBodySchema>
