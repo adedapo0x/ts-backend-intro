a628e3596eecd719d78561687b68a9d37b47fd8b
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-29 20:52:20 +0100

    began implementation of getUserSessions

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,7 +1,7 @@
-import {validatePassword} from "../middlewares/password-validate";
-import {Request, Response } from "express"
-import Session from "../models/session.model";
-import {signJWT} from "../utils/jwt";
+import { validatePassword } from "../middlewares/password-validate"
+import { Request, Response } from "express"
+import Session from "../models/session.model"
+import { signJWT } from "../utils/jwt"
 import config from "config"
 import logger from "../utils/logger"
 import { createSessionInput } from "../schema/session.schema"
@@ -11,24 +11,34 @@ export const createSession = async (userID:string, userAgent:string) => {
     return session.toJSON()
 }
 
-export const createUserSession = async (req: Request, res: Response) => {
+export const createUserSession = async (req: Request<{}, {}, createSessionInput>, res: Response) => {
     try {
         // Validates user password
         const user = await validatePassword(req.body)
-        if (!user) return res.status(401).json({message: "Invalid email or password"})
+        if (!user) {
+            res.status(401).json({message: "Invalid email or password"})
+        }
+        else {
+            // create session
+            const session = await createSession(user._id.toString(), req.get("user-agent") || "")
 
-        // create session
-        const session = await createSession(user._id.toString(), req.get("user-agent") || "")
+            // create access token
+            const accessToken = signJWT({userId: user._id, session: session._id}, {expiresIn: config.get("accessTokenTtl")})
 
-        // create access token
-        const accessToken = signJWT({...user, session: session._id}, {expiresIn: config.get("accessTokenTtl")})
-
-        // create refresh Token
-        const refreshToken = signJWT({...user, session: session._id}, {expiresIn: config.get("refreshTokenTtl")})
-        return res.json({accessToken, refreshToken})
+            // create refresh Token
+            const refreshToken = signJWT({userId: user._id, session: session._id}, {expiresIn: config.get("refreshTokenTtl")})
+            res.json({accessToken, refreshToken})
+        }
 
     } catch (e) {
         logger.error("Error creating user session")
-        return res.status(400).json({message: "Error encountered!"})
+        res.status(400).json({message: "Error encountered!"})
     }
+}
+
+export const getUserSessions = async (req: Request, res: Response) => {
+    const userID = res.locals.user
+
+
+
 }
