59a1b2a3ec524a15175de362fbeef7bb285299d9
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-30 18:31:48 +0100

    chore: implemented the deleteSession handler

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -7,8 +7,12 @@ import logger from "../utils/logger"
 import { createSessionInput } from "../schema/session.schema"
 
 export const createSession = async (userID:string, userAgent:string) => {
-    const session = await Session.create({user: userID, userAgent})
-    return session.toJSON()
+    try{
+        const session = await Session.create({user: userID, userAgent})
+        return session.toJSON()
+    } catch (e){
+        logger.error("Error encountered while creating session")
+    }
 }
 
 export const createUserSession = async (req: Request<{}, {}, createSessionInput>, res: Response) => {
@@ -22,25 +26,52 @@ export const createUserSession = async (req: Request<{}, {}, createSessionInput>
             // create session
             const session = await createSession(user._id.toString(), req.get("user-agent") || "")
 
-            // create access token
-            const accessToken = signJWT({userId: user._id, session: session._id}, {expiresIn: config.get("accessTokenTtl")})
+            if (!session){
+                res.status(400).json({message: "Session not created"})
+            } else {
+                // create access token
+                const accessToken = signJWT({userId: user._id, session: session._id}, {expiresIn: config.get("accessTokenTtl")})
 
-            // create refresh Token
-            const refreshToken = signJWT({userId: user._id, session: session._id}, {expiresIn: config.get("refreshTokenTtl")})
-            res.json({accessToken, refreshToken})
+                // create refresh Token
+                const refreshToken = signJWT({userId: user._id, session: session._id}, {expiresIn: config.get("refreshTokenTtl")})
+                res.json({accessToken, refreshToken})
+            }
         }
 
     } catch (e) {
-        logger.error("Error creating user session")
+        logger.error("Error creating user session: ", e)
         res.status(400).json({message: "Error encountered!"})
     }
 }
 
 export const getUserSessions = async (req: Request, res: Response) => {
-    const userID = res.locals.user.userId
+    try {
+        const userID = res.locals.user.userId
 
-    const sessions = await Session.find({user: userID, valid: true})
+        const sessions = await Session.find({user: userID, valid: true})
 
-    res.json({sessions})
+        res.json({sessions})
+    } catch (e) {
+        logger.error("Error getting user sessions: ", e)
+        res.status(400).json({message: "Error getting user sessions!"})
+    }
+
+
+}
+
+export const deleteSession = async (req: Request, res: Response) => {
+    try{
+        const session = res.locals.user.session
+
+        await Session.updateOne({_id: session}, {valid: false})
+
+        res.send({
+            accessToken: null,
+            refreshToken: null
+        })
+    } catch (e) {
+        logger.error("Error logging out: ", e)
+        res.status(400).json({message: "Error while logging out!"})
+    }
 
 }
