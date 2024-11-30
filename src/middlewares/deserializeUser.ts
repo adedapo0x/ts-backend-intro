2410b4b14c42dee112b509de29db002ada5e7713
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-30 18:18:45 +0100

    added error handling for deserialize Middleware

--- src/middlewares/deserializeUser.ts
+++ src/middlewares/deserializeUser.ts
@@ -1,6 +1,7 @@
 import { get } from "lodash"
 import { Request, Response, NextFunction } from "express"
 import {verifyJWT} from "../utils/jwt";
+import logger from "../utils/logger";
 
 export const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
     const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "")
@@ -8,11 +9,15 @@ export const deserializeUser = (req: Request, res: Response, next: NextFunction)
     if (!accessToken) {
         next()
     }
-    const { decoded, expired } = verifyJWT(accessToken)
-    if (decoded) {
-        res.locals.user = decoded
+    try{
+        const { decoded, expired } = verifyJWT(accessToken)
+        if (decoded) {
+            res.locals.user = decoded
+        }
         next()
+    } catch (e){
+        logger.error("Error verifying JWT: ", e)
+        res.status(400).json({message: "Error verifying JWT"})
     }
-
 
 }
