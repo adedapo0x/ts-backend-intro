47faccf363e0e8ebb2616bc5de77a32008356327
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-01 18:35:03 +0100

    added accessToken regeneration from refreshToken logic

--- src/middlewares/deserializeUser.ts
+++ src/middlewares/deserializeUser.ts
@@ -1,20 +1,57 @@
 import { get } from "lodash"
 import { Request, Response, NextFunction } from "express"
-import {verifyJWT} from "../utils/jwt";
+import {signJWT, verifyJWT} from "../utils/jwt";
 import logger from "../utils/logger";
+import Session from "../models/session.model";
+import User from "../models/user.models";
+import config from "config";
 
-export const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+
+const reIssueAccessToken = async ({refreshToken}: {refreshToken: string}) => {
+    const decoded = verifyJWT(refreshToken)
+    if (!decoded || !get(decoded, "session")) return false
+
+    const session = await Session.findById(get(decoded, "session"))
+    if (!session || !session.valid) return false
+
+    const user = await User.findOne({_id: session.user}).lean()
+
+    if (!user) return false
+
+    const accessToken = signJWT(
+        {userId: user._id, session: session._id},
+        {expiresIn: config.get("accessTokenTtl")}
+    )
+    return accessToken
+}
+
+export const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/, "")
-
+    const rawRefreshToken = get(req, "headers.x-refresh")
+    const refreshToken = Array.isArray(rawRefreshToken) ? rawRefreshToken[0] : rawRefreshToken
     if (!accessToken) {
         next()
     }
     try{
         const { decoded, expired } = verifyJWT(accessToken)
-        if (decoded) {
+        if (decoded && !expired) {
             res.locals.user = decoded
+            next()
         }
-        next()
+        if (expired && refreshToken){
+            const newAccessToken = await reIssueAccessToken({ refreshToken })
+            if (!newAccessToken){
+                res.status(403).json({message: "Error confirming authorization"})
+            }
+            else {
+                res.setHeader("x-access-token", newAccessToken)
+
+                const result = verifyJWT(newAccessToken)
+                res.locals.user = result.decoded
+                next()
+            }
+            next()
+        }
     } catch (e){
         logger.error("Error verifying JWT: ", e)
         res.status(400).json({message: "Error verifying JWT"})
