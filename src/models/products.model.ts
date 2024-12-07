b2f19cd2202d9fdfed3bcfb6090835a850e7eb2e
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-07 19:42:12 +0100

    created model for products

---
+++ src/models/products.model.ts
@@ -0,0 +1,24 @@
+import mongoose, {Document, Schema, model} from 'mongoose'
+import {IUserDocument} from "./user.models";
+
+interface IProduct extends Document {
+    user: IUserDocument["_id"]
+    title: string
+    description: string
+    price: number
+    image: string
+}
+
+const productSchema = new Schema<IProduct>({
+    user: {type: Schema.Types.ObjectId, ref: "User"},
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    image: {type: String, required: true},
+}, {
+    timestamps: true
+})
+
+const productModel = model<IProduct>("Product", productSchema)
+
+export default productModel
