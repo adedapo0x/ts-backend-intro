62ae68eabd36fa20dbc3532600561ca4ad0d5667
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-09 18:46:00 +0100

    finished validation for product request handlers

--- src/schema/product.schema.ts
+++ src/schema/product.schema.ts
@@ -1,11 +1,40 @@
-import {object, string, TypeOf } from "zod"
+import {object, string, number, TypeOf } from "zod"
 
 
 const payload = object({
     title: string({
-        required_error: "Title must be a string"
+        required_error: "Title cannot be empty"
     }).min(2, "Title must be at least 2 characters long"),
     description: string({
-        required_error: "Description must be a string"
-    }).min(120, "Description must contain at least 120 characters")
+        required_error: "Description cannot be empty"
+    }).min(120, "Description must contain at least 120 characters"),
+    price: number({
+        required_error: "Price of product must be included",
+        invalid_type_error: "Price must be a number"
+    }),
+    image: string({
+        required_error: "Image cannot be empty and must be a string"
+    })
 })
+
+const paramsPayload = object({
+    _id: string({
+        required_error: "Product ID is required"
+    })
+})
+
+export const productSchema = {
+    body: payload,
+    params: paramsPayload
+}
+
+export const createProductSchema = object({ payload })
+export const updateProductSchema = object({ payload, paramsPayload })
+export const getProductSchema = object({ paramsPayload })
+export const deleteProductSchema = object({ paramsPayload })
+
+
+export type createProductInput = TypeOf<typeof createProductSchema>
+export type updateProductInput = TypeOf<typeof updateProductSchema>
+export type getProductInput = TypeOf<typeof getProductSchema>
+export type deleteProductInput = TypeOf<typeof deleteProductSchema>
