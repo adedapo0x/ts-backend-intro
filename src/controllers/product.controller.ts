5e2c2ed1992853296922e063b90ce6fb49b41361
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-15 18:39:48 +0100

    added getProduct and deleteProduct handlers

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import {createProductInput, updateProductInput} from "../schema/product.schema";
+import {createProductInput, deleteProductInput, getProductInput, updateProductInput} from "../schema/product.schema";
 import Product from "../models/products.model";
 import logger from "../utils/logger";
 
@@ -15,7 +15,21 @@ export const createProduct = async (req: Request<{},{},createProductInput>, res:
     }
 }
 
-export const updateProduct = async (req: Request<updateProductInput["paramsPayload"]>, res: Response) => {
+export const getProduct = async (req: Request<getProductInput["paramsPayload"]>, res: Response) => {
+    try{
+        const productId = req.params._id
+        const product = await Product.findById(productId)
+        if (!product) {
+            res.status(404).json({message: "Product Not Found"})
+        } else {
+            res.json({product})
+        }
+    } catch (e) {
+        logger.error("Error while fetching product: ", e)
+    }
+}
+
+export const updateProduct = async (req: Request<updateProductInput["paramsPayload"], {}, updateProductInput["payload"]>, res: Response) => {
     try{
         const userID = res.locals.user._id
         const productID = req.params._id
@@ -32,5 +46,23 @@ export const updateProduct = async (req: Request<updateProductInput["paramsPaylo
         }
     } catch (e) {
         logger.error("Error updating product", e)
+    }
+}
+
+export const deleteProduct = async (req: Request<deleteProductInput["paramsPayload"]>, res: Response) => {
+    try{
+        const userID = res.locals.user._id
+        const productID = req.params._id
+
+        const product = await Product.findById(productID)
+        if (!product){
+            res.status(404).json({message: "Product not found"})
+        } else if (product.user != userID) {
+            res.status(403).json({message: "Unauthorized access"})
+        } else {
+            await Product.deleteOne({_id: productID})
+        }
+    } catch (e) {
+        logger.error("Error deleting product", e)
     }
 }
